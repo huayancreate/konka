cf6d5431835d75573716b68e29bfe36dfd57fb12
hywliu <apple@appletekiMacBook-Pro.local>
2013-07-26 15:47:15 +0800

    增加About页面

---
+++ KONKA_MARKET/KONKA_MARKET/ViewController/AboutView/HYAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  HYAboutViewController.h
+//  KONKA_MARKET
+//
+//  Created by andychen on 13-7-26.
+//  Copyright (c) 2013年 archon. All rights reserved.
+//
+
+#import "HYBaseViewController.h"
+
+@interface HYAboutViewController : HYBaseViewController
+
+@end
